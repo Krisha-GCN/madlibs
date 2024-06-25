0a265d6884f62e415c4467fbd3f2670a8beef67e
breadpandedpan <krisha.agarwal@csedge.org>
2024-06-25 14:45:23 -0700

    wordleV1

---
+++ words.py
@@ -0,0 +1,25 @@
+#wordle
+
+
+#def word(letters)
+word = ["s","l","a","t","e"]
+"""function that passes in letters list and maps/checks to og one??"""
+
+letters = ["_", "_", "_", "_", "_"]
+print(letters)
+
+#letter1 = letters[0]
+#letter2 = "_"
+#letter3 = "_"
+#letter4 = "_"
+#letter5 = "_"
+
+guessword1 = list(input("First word guess: "))
+#guessed word becomes list of each letter
+
+"""checking length of guessword1, if too long redo?"""
+#if len(guessword1) > 5:
+#    print("too long")
+#else:
+#    print("ok moving on")
+
